54009dfb995938f2dd91266cda47fab581998ca4
Feke András <afeke@pollak.hu>
2025-02-22 13:41:28 +0100

    refactor: add port configuration to database connection for improved flexibility

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,6 +2,7 @@ import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT || "3306"),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
